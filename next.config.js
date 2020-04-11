8692d48fc44032740a245f44def53fe20a8f0ec5
Kevin Sparling <kevinsparling@gmail.com>
2020-04-11 12:57:20 +0800

    fixing the session variables

--- next.config.js
+++ next.config.js
@@ -89,7 +89,7 @@ module.exports = withCSS(withPurgeCss({
 {
   env: {
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-    AUTH0_CLIENTID: process.env.AUTH0_CLIENTID,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
     AUTH0_SCOPE: 'openid profile',
     REDIRECT_URI: process.env.REDIRECT_URI || 'http://localhost:3000/api/callback',
